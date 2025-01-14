596838246f232e80e43af66edb2ac055120bd735
baguschys <bagus.cahyo@movingbytesplayground.com>
2025-01-14 19:46:13 +0700

    fix data  vendor

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vendor extends Model
 {
-    const CATEGORI_VANUE = 'Vanue';
+    const CATEGORI_VANUE = 'Venue';
     const CATEGORI_CATERING = 'Catering';
     const CATEGORI_DECORATION = 'Decoration';
     const CATEGORI_PHOTO_AND_VIDEO = 'Photo & Video';
